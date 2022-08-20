34a429f4a87a6ae04118123b19b763e537860e20
drayns20 <drayns203@gmail.com>
2022-08-20 19:05:13 +0300

    minor changes to carousel

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></img></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></img></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></img></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></img></button>',
         responsive: [
             {
                 breakpoint: 992,
