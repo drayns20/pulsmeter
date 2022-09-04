b737a8130ff58a2d522f2c13da9db93f182d3c56
drayns20 <drayns203@gmail.com>
2022-09-04 10:15:52 +0300

    added mailer

--- src/js/script.js
+++ src/js/script.js
@@ -51,6 +51,131 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn('slow');
         });
      });
-     
 
+    //  $('#consultation-form').validate({
+    //     rules: {
+    //         name: {
+    //             required: true,
+    //             minlength: 2
+    //         },
+    //         phone: "required",
+    //         email: {
+    //             required: true,
+    //             email: true
+    //         }
+    //     },
+    //     messages: {
+    //         name: {
+    //             required: "Пожалуйста введите своё имя",
+    //             minlength: jQuery.validator.format("Имя должно включать как минимум {0} символа!")
+    //         },
+    //         phone: "Пожалуйста введите свой номер телефона",
+    //         email: {
+    //           required: "Пожалуйста введите свой email чтобы мы могли с вами связаться",
+    //           email: "Ваш email должен быть в формате name@domain.com"
+    //         }
+    //       }
+    //  });
+    //  $('#consultation form').validate({
+    //     rules: {
+    //         name: {
+    //             required: true,
+    //             minlength: 2
+    //         },
+    //         phone: "required",
+    //         email: {
+    //             required: true,
+    //             email: true
+    //         }
+    //     },
+    //     messages: {
+    //         name: {
+    //             required: "Пожалуйста введите своё имя",
+    //             minlength: jQuery.validator.format("Имя должно включать как минимум {0} символа!")
+    //         },
+    //         phone: "Пожалуйста введите свой номер телефона",
+    //         email: {
+    //           required: "Пожалуйста введите свой email чтобы мы могли с вами связаться",
+    //           email: "Ваш email должен быть в формате name@domain.com"
+    //         }
+    //       }
+    //  });
+    //  $('#order form').validate({
+    //     rules: {
+    //         name: {
+    //             required: true,
+    //             minlength: 2
+    //         },
+    //         phone: "required",
+    //         email: {
+    //             required: true,
+    //             email: true
+    //         }
+    //     },
+    //     messages: {
+    //         name: {
+    //             required: "Пожалуйста введите своё имя",
+    //             minlength: jQuery.validator.format("Имя должно включать как минимум {0} символа!")
+    //         },
+    //         phone: "Пожалуйста введите свой номер телефона",
+    //         email: {
+    //           required: "Пожалуйста введите свой email чтобы мы могли с вами связаться",
+    //           email: "Ваш email должен быть в формате name@domain.com"
+    //         }
+    //       }
+    //  });
+
+     function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста введите своё имя",
+                    minlength: jQuery.validator.format("Имя должно включать как минимум {0} символа!")
+                },
+                phone: "Пожалуйста введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста введите свой email чтобы мы могли с вами связаться",
+                  email: "Ваш email должен быть в формате name@domain.com"
+                }
+              }
+         });
+     };
+     
+     validateForms('#consultation-form');
+     validateForms('#consultation form');
+     validateForms('#order form');
+
+     $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+     $('form').submit(function(e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+            }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
